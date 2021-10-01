80867a6fe06809ef4c2de60f515218a9096dbcc1
Andre Schinkel <doccodyblue@gmail.com>
2021-10-01 07:37:53 +0200

    added frames per minute metric

--- aisreporter.py
+++ aisreporter.py
@@ -2,16 +2,18 @@ import serial
 import socket
 import prometheus_client as prom
 import logging
+import time
 from datetime import datetime
 from configparser import ConfigParser
 from aisjson import AisAprs
+from statistics import mean
 
 
 config = ConfigParser()
 config.read("aisreporter.ini")
 
 
-def ConfigSectionMap(section):
+def configsectionmap(section):
     dict1 = {}
     options = config.options(section)
     for option in options:
@@ -23,37 +25,36 @@ def ConfigSectionMap(section):
     return dict1
 
 
-debug = eval(ConfigSectionMap("generic")['debug'])
+debug = eval(configsectionmap("generic")['debug'])
 if debug == 1:
     logging.basicConfig(level=logging.DEBUG)
 else:
     logging.basicConfig(level=logging.WARNING)
 
-metrics = eval(ConfigSectionMap("generic")['metrics'])
+metrics = eval(configsectionmap("generic")['metrics'])
 if metrics == 1:
-    prometheusport = eval(ConfigSectionMap("generic")['metricsport'])
+    prometheusport = eval(configsectionmap("generic")['metricsport'])
 
-marinetrafficenabled = eval(ConfigSectionMap("marinetraffic")['enabled'])
+marinetrafficenabled = eval(configsectionmap("marinetraffic")['enabled'])
 if marinetrafficenabled == 1:
-    marinetrafficip = ConfigSectionMap("marinetraffic")['ip']
-    marinetrafficport = eval(ConfigSectionMap("marinetraffic")['port'])
+    marinetrafficip = configsectionmap("marinetraffic")['ip']
+    marinetrafficport = eval(configsectionmap("marinetraffic")['port'])
 
-aishubenabled = eval(ConfigSectionMap("aishub")['enabled'])
+aishubenabled = eval(configsectionmap("aishub")['enabled'])
 if aishubenabled == 1:
-    aishubip = ConfigSectionMap("aishub")['ip']
-    aishubport = eval(ConfigSectionMap("aishub")['port'])
+    aishubip = configsectionmap("aishub")['ip']
+    aishubport = eval(configsectionmap("aishub")['port'])
 
-aprsenabled = eval(ConfigSectionMap("aprs")['enabled'])
+aprsenabled = eval(configsectionmap("aprs")['enabled'])
 if aprsenabled == 1:
-    aprsurl = ConfigSectionMap("aprs")['url']
-    aprsname = ConfigSectionMap("aprs")['name']
+    aprsurl = configsectionmap("aprs")['url']
+    aprsname = configsectionmap("aprs")['name']
 
-serialport = ConfigSectionMap("generic")['serialport']
-serialbaud = eval(ConfigSectionMap("generic")['serialbaud'])
+serialport = configsectionmap("generic")['serialport']
+serialbaud = eval(configsectionmap("generic")['serialbaud'])
 
 
 class SendAIS:
-    # todo error handling if network is not reachable
     def __init__(self, ip, port):
         self.sentPackets = 0
         self.UDP_IP = ip
@@ -61,8 +62,14 @@ class SendAIS:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def sendframe(self, packetdata):
-        self.sock.sendto(bytes(packetdata, "ASCII"), (self.UDP_IP, self.UDP_PORT))
-        self.sentPackets += 1
+        try:
+            self.sock.sendto(bytes(packetdata, "ASCII"), (self.UDP_IP, self.UDP_PORT))
+            self.sentPackets += 1
+        except socket.error:
+            timeprint('E: socket.error in sendframe - waiting for 60 sec ')
+            time.sleep(60)
+        except Exception as e:
+            timeprint(e)
 
 
 class MetricsAis:
@@ -70,6 +77,7 @@ class MetricsAis:
         self.aissent = prom.Counter('ais_frames_forwarded', 'AIS packets forwarded')
         self.aiserror = prom.Counter('ais_decode_errors', 'AIS decode errors')
         self.aismissingmulti = prom.Counter('ais_decode_missingmultipart', 'AIS missing multipart')
+        self.packetsperminute = prom.Gauge('ais_packets_per_minute', 'AIS received ppm rate')
         prom.start_http_server(port)
 
     def incais(self, value):
@@ -80,6 +88,33 @@ class MetricsAis:
 
     def incmissingmulti(self, value):
         self.aismissingmulti.inc(value)
+
+    def packetrate(self, value):
+        self.packetsperminute.set(value)
+
+
+class ThingsPerMinute:
+    def __init__(self):
+        self.timestamp = datetime.today().timestamp()
+        self.timestamp_last = self.timestamp
+        self.rate = 0
+        self.timeperpacketaverage = []
+
+    def update(self, inc):
+        timestamp_now = datetime.today().timestamp()
+        timedifference = timestamp_now - self.timestamp_last
+        self.timeperpacketaverage.append(timedifference)
+
+        self.timestamp_last = timestamp_now
+        if len(self.timeperpacketaverage) > 100:
+            self.timeperpacketaverage.pop(0)
+
+        self.rate = round(60 / mean(self.timeperpacketaverage), 1)
+        return self.rate
+
+    def ask(self):
+        return self.rate
+
 
 def timeprint(text):
     print(datetime.now(), ': ', text)
@@ -106,7 +141,9 @@ if aprsenabled:
 if metrics == 1:
     metric = MetricsAis(prometheusport)
     timeprint('Metrics enabled')
-    
+
+rate = ThingsPerMinute()
+
 while 1:
     line = daisy.readline().decode('ASCII')
     if line[0:6] == '!AIVDM':
@@ -126,9 +163,12 @@ while 1:
                 aprs.sendframe(jsonaprs)
                 timeprint('S: APRS.fi')
 
+        rate.update(1)
 
         if metrics == 1:
             metric.incais(1)
+            x = rate.ask()
+            metric.packetrate(x)
 
         logging.debug('received frame: %s', line)
     elif line[0:16] == 'error: RSSI drop':
